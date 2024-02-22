0c02f9389a9431e8ab940d5d23a9c96e2ef5e9ef
sperper2907 <sperper2907@g.educaand.es>
2024-02-22 11:32:18 +0100

    Sperper - Ejercicio 1 casi finalizado y ejercicio 2 empezado

---
+++ SamPer_Ej2-ExUD4/src/main/java/com/mycompany/samper_ej2/exud4/SamPer_Ej2ExUD4.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.samper_ej2.exud4;
+
+/**
+ *
+ * @author sperper2907
+ */
+public class SamPer_Ej2ExUD4 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ unit-testing-using-nunit/PrimeService/PrimeService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace PrimeService;
+
+public class PrimeService
+{
+    public bool isPrime(int candidate)
+    {
+        throw new NotImplementedException("Please create a test first");
+    }
+}
